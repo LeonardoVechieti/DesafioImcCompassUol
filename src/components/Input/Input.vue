11007fb566a465959f3fd89c3c33a18410cbf174
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-08-18 21:18:52 -0300

    Feat: input component

---
+++ src/components/Input/Input.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="input">
+    <label>{{ label }}</label>
+    <output>{{ output }}</output>
+    <input
+      type="range"
+      :id="inputId"
+      :min="minRange"
+      :max="maxRange"
+      :step="step"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line
+    name: "Input",
+    props: {
+        label: {
+            type: String,
+            default: "Input"
+        },
+        inputId: {
+            type: String,
+            default: "input"
+        },
+        minRange: {
+            type: Number,
+            default: 0
+        },
+        maxRange: {
+            type: Number,
+            default: 100
+        },
+        step: {
+            type: Number,
+            default: 1
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Imc/Imc.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <div class="home-card">
+      <p class="title-card">Calcule seu IMC</p>
+      <div class="inputs">
+        <Input />
+      </div>
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+import Input from "@/components/Input/Input.vue";
+//import Tabs from "@/components/containers/Tabs/Tabs.vue";
+export default {
+  // eslint-disable-next-line
+  name: "Imc",
+  components: {
+    Input
+  },
+  
+};
+</script>
+
+<style lang="scss" scoped>
+
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: "Poppins", sans-serif;
+  background: linear-gradient(217deg, #F7B718 , #F5C54E 70.71%),
+}
+.home-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #F8EBC8;
+    border: none;
+    border-radius: 10px;
+    width: 35%;
+  }      
+  .title-card {
+    font-size: 1.5rem;
+    text-align: center;
+ 
+  }    
+</style>
